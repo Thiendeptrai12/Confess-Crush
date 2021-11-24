c6be368696208dd62e388a1eeead3dd5e19156a1
Thiendeptrai12 <94940067+Thiendeptrai12@users.noreply.github.com>
2021-11-24 10:42:37 +0700

    Update config.js

--- config.js
+++ config.js
@@ -8,13 +8,13 @@ const CONFIG = {
     btnIntro: '^^HiHi^^',
     title: 'Phải chăng Thịnh đã yêu Loan ngay từ cái nhìn đầu tiên 🥰',
     desc: 'Phải chăng Loan đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
+    btnYes: 'Loan yêu thịnh mà đúng không <33',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    question: 'Trên thế giới hơn 3,5 tỉ người  là nữ mà sao thịnh lại yêu loan <3',
     btnReply: 'Gửi cho bạn <3',
     reply: 'Tại vì loan và thịnh sinh ra là dành cho nhau mà <33333333',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
+    messDesc: 'Hãy chúc cho cặp đôi tụi mình hạnh phúc nhé :33.',
+    btnAccept: 'Okiiiii lun    -Thiện Đz-<3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
